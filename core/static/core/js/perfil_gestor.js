871cc632e92ac60d9906330c60058a0ac97ff5d9
caio carvalho - senai <caio.s.carvalho@aluno.senai.br>
2025-10-08 14:21:16 -0300

    mudanças

---
+++ core/static/core/js/perfil_gestor.js
@@ -0,0 +1,10 @@
+import { BASE_URL, ApiData } from "/static/js/api.js";
+const input  = document.getElementById('profilePicInput')
+const img  = document.getElementById('imgcontainer')
+input.addEventListener('change', () => {
+    const file = input.files[0];
+    if (file) {
+        const imageUrl = URL.createObjectURL(file);
+        img.src = imageUrl;
+    }
+});
